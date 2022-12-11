d6c84742c95ac89404555cb14c8865c44a0d0e85
Sadik Ekin Ozbay <ekin.oezbay@tum.de>
2022-12-11 13:23:29 +0100

    Remove unused Env variable

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NFT_STORAGE_KEY: process.env.NFT_STORAGE_KEY,
     SIGNER_MNEMONIC: process.env.SIGNER_MNEMONIC,
     WITHDRAW_ACCOUNT: process.env.WITHDRAW_ACCOUNT,
     INFURA_IPFS_PROJECT_ID: process.env.INFURA_IPFS_PROJECT_ID,
