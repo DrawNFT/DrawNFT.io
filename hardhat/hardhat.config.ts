498f1e00df2d86b748c18c07b2b472382a316de7
Sadik Ekin Ozbay <ekin.oezbay@tum.de>
2022-12-28 13:56:25 +0100

    New Goerli contract and readme updates

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 require('dotenv').config();
-const { GOERLI_TEST_PRIVATE_KEY, ALCHEMY_GOERLI_API_KEY } = process.env;
+const { GOERLI_TEST_PRIVATE_KEY, ALCHEMY_GOERLI_API_KEY, ETH_MAIN_PRIVATE_KEY, ALCHEMY_MAIN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
@@ -16,6 +16,11 @@ const config: HardhatUserConfig = {
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_GOERLI_API_KEY}`,
       accounts: [GOERLI_TEST_PRIVATE_KEY || ''],
       chainId: 5,
+    },
+    main: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_MAIN_API_KEY}`,
+      accounts: [ETH_MAIN_PRIVATE_KEY || ''],
+      chainId: 1,
     },
   },
 };
