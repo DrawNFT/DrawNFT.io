0eb99b23abb2523fa7e12f156680e46bfa842a7f
Sadik Ekin Ozbay <ekin.oezbay@tum.de>
2022-12-28 09:29:41 +0100

    Adjust generation attribute and account store wallet connect popup

--- components/utils/useAccountStore.ts
+++ components/utils/useAccountStore.ts
@@ -13,6 +13,10 @@ export const useAccountStore = create<AccountState>()((set) => ({
         method: 'eth_requestAccounts',
       });
       set({ account: accounts[0] });
+    } else {
+      alert(
+        'To interact with this application, it is necessary to have a web3 wallet such as MetaMask. This will enable you to securely connect to the decentralized network and interact with the app. Please ensure that you have a web3 wallet installed and configured before attempting to use this application.'
+      );
     }
   },
 }));
